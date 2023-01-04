4a7d254f894423ab94773398b5bd3393a568a8ae
Vinicius Andrade <58332861+dev-vinicius-andrade@users.noreply.github.com>
2023-01-04 02:33:54 -0300

    empty manager api

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # snake-game
+
+
+# Nullable
+
+All projects of this solutions has the **tag** ```xml <Nullable> disable</Nullable>``` in the .csproj file. 
+This is because I'm using the **Nullable Reference Types** feature of C# 8.0. If you want to use this feature, you need to enable it in the .csproj file.
---
+++ src/Library/Library.Commons.Api/Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Library.Commons.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+[AllowAnonymous]
+public class HealthController : Controller
+{
+    private readonly HealthCheckService _healthCheckService;
+
+    public HealthController(HealthCheckService healthCheckService)
+    {
+        this._healthCheckService = healthCheckService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Get()
+    {
+        HealthReport report = await this._healthCheckService.CheckHealthAsync();
+        var result = new
+        {
+            status = report.Status.ToString(),
+            errors = report.Entries.Select(e => new { name = e.Key, status = e.Value.Status.ToString(), description = e.Value.Description?.ToString() ?? e.Value.Status.ToString() })
+        };
+        return report.Status == HealthStatus.Healthy ? Ok(result) : StatusCode((int)HttpStatusCode.ServiceUnavailable, result);
+    }
+}
